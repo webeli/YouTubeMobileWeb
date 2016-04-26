d7c984fc3f101f66066957a574badcac41895cf4
Elias Hansen <pe_lias@msn.com>
2016-04-27 01:54:48 +0200

    style changes and implemented youtube api

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,14 +1,35 @@
 angular.module('app.controllers', [])
-  
-.controller('searchCtrl', function($scope) {
+
+.controller('searchCtrl', function($scope, $http) {
+
+  $scope.videos = [];
+
+  $scope.youtubeParams = {
+    key: 'AIzaSyCNoxTc2M1WvYikPfm7NhYapmWTYq9ye5A',
+    type: 'video',
+    maxResults: '20',
+    part: 'id,snippet',
+    q: '',
+    order: 'relevance'
+  }
+
+  $scope.getVideos = function(query) {
+    $scope.youtubeParams.q = query;
+    $scope.videos = [];
+    $http.get('https://www.googleapis.com/youtube/v3/search', {params:$scope.youtubeParams}).success(function(response){
+      angular.forEach(response.items, function(child){
+        console.log (child);
+        $scope.videos.push(child);
+      });
+    });
+  };
 
 })
-   
+
 .controller('myMusicCtrl', function($scope) {
 
 })
-   
+
 .controller('exploreCtrl', function($scope) {
 
 })
-    
