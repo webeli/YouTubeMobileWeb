7f6e03f4c445daeafa4122f5ff2cd8868cd0df90
earthquaker <pe_lias@msn.com>
2016-04-28 14:26:48 +0200

    implemented spinner when searching

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,25 +1,31 @@
 angular.module('app.controllers', [])
 
-.controller('searchCtrl', function($scope, $http) {
+.controller('searchCtrl', function($scope, $http, $q, $ionicLoading) {
 
   $scope.videos = [];
 
   $scope.youtubeParams = {
     key: 'AIzaSyCNoxTc2M1WvYikPfm7NhYapmWTYq9ye5A',
     type: 'video',
-    maxResults: '20',
+    maxResults: '50',
     part: 'id,snippet',
     q: '',
     order: 'relevance'
   }
 
   $scope.getVideos = function(query) {
+    $ionicLoading.show({
+      template: '<p>Loading ' + query + '...</p><ion-spinner></ion-spinner>'
+    });
     $scope.youtubeParams.q = query;
     $scope.videos = [];
     $http.get('https://www.googleapis.com/youtube/v3/search', {params:$scope.youtubeParams}).success(function(response){
       angular.forEach(response.items, function(child){
-        console.log (child);
         $scope.videos.push(child);
+      });
+      $q.all($scope.videos).then(function () {
+        console.log($scope.videos);
+        $ionicLoading.hide();
       });
     });
   };
